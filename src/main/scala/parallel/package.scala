cdcf8f381af2827de06790af47a24c6110a45e57
Mukhamed Karanashev <menchauser@gmail.com>
2016-12-10 12:05:21 +0300

    Use ExecutorContext as implementation for Par

--- src/main/scala/parallel/package.scala
+++ src/main/scala/parallel/package.scala
@@ -1,19 +1,74 @@
+
+import java.util.concurrent._
+
 package object parallel {
 
-  class Par[A] {
-
-  }
+  type Par[A] = ExecutorService => Future[A]
 
   object Par {
-    def unit[A](a: A): Par[A] = ???
+    def unit[A](a: A): Par[A] = (_: ExecutorService) => UnitFuture(a)
+
 
     def lazyUnit[A](a: => A): Par[A] = fork(unit(a))
 
-    def map2[A, B, C](pa: Par[A], pb: Par[B])(f: (A, B) => C): Par[C] = ???
 
-    def fork[A](a: => Par[A]): Par[A] = ???
+    def asyncF[A, B](f: A => B): A => Par[B] =
+      a => lazyUnit(f(a))
 
-    def run[A](a: Par[A]): A = ???
+
+    def map2[A, B, C](pa: Par[A], pb: Par[B])(f: (A, B) => C): Par[C] =
+      (es: ExecutorService) => {
+        val af = pa(es)
+        val bf = pb(es)
+        UnitFuture(f(af.get, bf.get))
+      }
+
+
+    def fork[A](a: => Par[A]): Par[A] =
+      (es: ExecutorService) => {
+        es.submit(new Callable[A] {
+          override def call: A = a(es).get
+        })
+      }
+
+
+    def run[A](s: ExecutorService)(a: Par[A]): Future[A] = a(s)
+
+
+    private case class UnitFuture[A](get: A) extends Future[A] {
+      override def get(timeout: Long, unit: TimeUnit): A = get
+      override def cancel(evenIfRunning: Boolean): Boolean = false
+      override def isDone: Boolean = true
+      override def isCancelled: Boolean = false
+    }
+
+
+    private case class Map2Future[A, B, C](a: Future[A], b: Future[B],
+                                           f: (A, B) => C) extends Future[C] {
+      @volatile var cache: Option[C] = None
+
+      override def isDone: Boolean = cache.isDefined
+      override def isCancelled: Boolean = a.isCancelled || b.isCancelled
+      override def cancel(mayInterruptIfRunning: Boolean): Boolean =
+        a.cancel(mayInterruptIfRunning) || b.cancel(mayInterruptIfRunning)
+      override def get(): C = compute(Long.MaxValue)
+      override def get(timeout: Long, unit: TimeUnit): C =
+        compute(unit.toNanos(timeout))
+
+      private def compute(timeoutNanos: Long): C = cache match {
+        case Some(c) => c
+        case None =>
+          val start = System.nanoTime()
+          val ar = a.get(timeoutNanos, TimeUnit.NANOSECONDS)
+          val stop = System.nanoTime()
+          val aTime = stop - start
+          val br = b.get(timeoutNanos - aTime, TimeUnit.NANOSECONDS)
+          val res = f(ar, br)
+          cache = Some(res)
+          res
+      }
+    }
+
   }
 
 
